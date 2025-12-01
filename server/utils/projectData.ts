0604fbb9f059302411fc0b9410d6ade1bf5414e6
bufang <1931297+budaobu@users.noreply.github.com>
2025-12-01 14:58:28 +0800

    Update projectData.ts

--- server/utils/projectData.ts
+++ server/utils/projectData.ts
@@ -12,7 +12,7 @@ export interface Project {
 // 单一数据源
 export const projects: Project[] = [
   {
-    id: 6,
+    id: 7,
     title: '视频转Gif',
     description: '在浏览器本地将视频转换为 GIF 动图，无需上传服务器。。',
     icon: '🎥',
@@ -21,7 +21,7 @@ export const projects: Project[] = [
     date: '2025-12-01'
   },
   {
-    id: 5,
+    id: 6,
     title: '作品集网站',
     description: '我的个人作品集网站，展示了我 vibe 的各种有趣项目和技术探索。',
     icon: '/api/avatar.png',
@@ -30,7 +30,7 @@ export const projects: Project[] = [
     date: '2025-12-01'
   },
   {
-    id: 4,
+    id: 5,
     title: '图片压缩助手',
     description: '图片压缩工具，支持 PNG, JPG, JPEG, WebP, AVIF 格式。浏览器本地压缩，无需上传服务器。',
     icon: '🖼',
@@ -39,7 +39,7 @@ export const projects: Project[] = [
     date: '2025-11-29'
   },
   {
-    id: 3,
+    id: 4,
     title: '表情包切片工具',
     description: '表情包制作工具，提供图片网格自动裁剪（支持6x4, 7x4等自定义布局）与GIF动图合成功能',
     icon: '✂',
@@ -48,7 +48,7 @@ export const projects: Project[] = [
     date: '2025-11-28'
   },
   {
-    id: 2,
+    id: 3,
     title: '证件照加水印',
     description: '证件照加水印工具，支持文字水印。可自定义位置、透明度和大小，操作简单，效果专业。',
     icon: '💻',
@@ -57,12 +57,21 @@ export const projects: Project[] = [
     date: '2025-01-05'
   },
   {
-    id: 1,
+    id: 2,
     title: '买电车还是油车？',
     description: '通过年行驶里程、充电条件、使用场景等多个维度，科学计算是否适合购买电动汽车或燃油车。',
     icon: '🚗',
     demoUrl: 'https://car-calculator.pages.dev/',
     githubUrl: 'https://github.com/budaobu/car-calculator',
     date: '2024-11-27'
+  },
+  {
+    id: 1,
+    title: 'ASN CN',
+    description: 'Fetch ASN CN data from bgp.he.net and whois.ipip.net daily.',
+    icon: '🌐',
+    demoUrl: '',
+    githubUrl: 'https://github.com/budaobu/rules/blob/main/py/cn-asn-scraper.py',
+    date: '2024-09-10'
   }
 ]
